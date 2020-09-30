73bbe70a4ef13f774ede5ffb7c011d14c15b970e
ijounjalkar <jounjalkarisha@gmail.com>
2020-09-30 21:04:39 +0530

    UC3_email commited

--- PatternMatcher.java
+++ PatternMatcher.java
@@ -6,25 +6,39 @@ import java.util.regex.Pattern;
 public class PatternMatcher {
 	public static void main(String[] args) {
 		PatternMatcher patternMatcher = new PatternMatcher();
-		String samplel = "Isha";
-		System.out.println(samplel+"------->"+patternMatcher.validateInput(samplel));
-		String samplel1 = "Jounjalkar";
-		System.out.println(samplel1+"------->"+patternMatcher.validateInput(samplel1));
+		String firstname = "Isha";
+		System.out.println(firstname+"------->"+patternMatcher.validateInput(firstname));
+		String lastname = "Jounjalkar";
+		System.out.println(lastname+"------->"+patternMatcher.validateInput(lastname));
+		String email = "abc.xyz@capg.co.in";
+		System.out.println(email+"------->"+patternMatcher.validateInput(email));
+	
 	}
 
-	private String validateInput(String samplel) {
+	private String validateInput(String firstname) {
 		String regex = "^[A-Z a-z]{3,25}$";
   	  Pattern pattern = Pattern.compile(regex);
-  	  Matcher matcher = pattern.matcher(samplel);
+  	  Matcher matcher = pattern.matcher(firstname);
   	  if(matcher.find()) {
   		  return "valid";
   		  
   	  }
   	  return "invalid";
     }
+	private String validateEmail(String email) {
+  	  String regex = "^(abc[.][A-Za-z]+@bl[.]co[.][A-Za-z]{3,25})$";
+  	  Pattern pattern = Pattern.compile(regex);
+  	  Matcher matcher = pattern.matcher(email);
+  	  if(matcher.find()) {
+  		  return "valid";
+  		  
+  	  }
+  	  return "invalid";
+    }
+	
 }
 		
-		// TODO Auto-generated method 
+		
 	
 
 
